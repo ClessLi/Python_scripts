b85fc89fe4afb22071266c088600a1fcdecdcd2d
ClessLi <316600949@qq.com>
2018-03-07 16:08:10 +0800

    更新目录为当前目录

--- 廖雪峰python课程/IO编程/操作文件和目录/search.py
+++ 廖雪峰python课程/IO编程/操作文件和目录/search.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python2.7
-#加载os、sys模块
 import os
 import sys
 
-#递归查询path下所有文件路径，并匹配关键字keyword
 def search_dir(keyword,path):
     for i in os.listdir(path):
         new_path=os.path.join(path,i)
@@ -13,10 +11,7 @@ def search_dir(keyword,path):
             if keyword in new_path:
                 print new_path[2:]
 
-#将程序第一个参数传给keyword
 keyword=sys.argv[1]
-#获取当前目录路径
 path='.'
 
-#运行程序
 search_dir(keyword,path)
