934190875b7bfa620e860eedd671853de82c7f87
ClessLi <316600949@qq.com>
2018-03-07 15:13:45 +0800

    学习IO编程，及通过递归查询当前目录下所有文件，并根据关键字匹配

---
+++ 廖雪峰python课程/IO编程/操作文件和目录/search.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2.7
+#加载os、sys模块
+import os
+import sys
+
+#递归查询path下所有文件路径，并匹配关键字keyword
+def search_dir(keyword,path):
+    for i in os.listdir(path):
+        new_path=os.path.join(path,i)
+        if os.path.isdir(new_path):
+            search_dir(keyword,new_path)
+        else:
+            if keyword in new_path:
+                print new_path[2:]
+
+#将程序第一个参数传给keyword
+keyword=sys.argv[1]
+#获取当前目录路径
+path=os.getcwd()
+
+#运行程序
+search_dir(keyword,path)
