f8fd3191cd7fd03888b281e30314bf1f5bdee06d
ClessLi <316600949@qq.com>
2018-03-13 00:03:53 +0800

    目录更新、图片信息查询脚本

---
+++ 廖雪峰python课程/15.常用內建模块/03.struct/题1/bmpinfo.py
@@ -0,0 +1,18 @@
+import sys
+import struct
+
+def checkbmp(file):
+    f_info=struct.unpack('<ccIIIIIIHH',file)
+    if f_info[0:2]==('B','M'):
+        print '''%s:
+        Size: %s x %s.
+        color: %s''' % (sys.argv[1],[6],f_info[7],f_info[9])
+    else:
+        print '%s is not a bmp file.' % sys.argv[1]
+
+try:
+    f=open(sys.argv[1],'r')
+
+finally:
+    if f:
+        f.close()
