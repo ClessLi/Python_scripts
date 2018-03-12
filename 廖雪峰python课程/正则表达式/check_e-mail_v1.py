ad8f76febaaf741bd4328f488eb1182431b3ae45
ClessLi <316600949@qq.com>
2018-03-12 20:09:08 +0800

    e-mail检查脚本完成

--- 廖雪峰python课程/正则表达式/check_e-mail_v1.py
+++ 廖雪峰python课程/正则表达式/check_e-mail_v1.py
@@ -4,23 +4,25 @@ import sys
 import re
 
 class e_mail():
-    def check(self,version,address):
-        if version=="1":
-            self.rule=re.compile(r'^([a-zA-Z0-9][a-zA-Z0-9\-\_\.]*?[a-zA-Z0-9])\@([a-zA-Z0-9][a-zA-Z0-9\-\_\.]*?[a-zA-Z0-9])\.(com|cn|org|net|us)$')
-            print '%s %s an e-mail address.' % (address,self.match(self.rule,address))
-        elif version=="2":
-            self.rule=re.compile(r'^(\<[a-zA-Z]+\s?[a-zA-Z]+\>)\s*([a-zA-Z0-9][a-zA-Z0-9\-\_\.]*?[a-zA-Z0-9])\@([a-zA-Z0-9][a-zA-Z0-9\-\_\.]*?[a-zA-Z0-9])\.(com|cn|org|net|us)$')
-            if self.match(self.rule,address)=="is":
-                print "%s %s an e-mail address and name is %s" (address,self.match(self.rule,address),self.rule.match(address).group(1))
+    def __init__(self):
+        self.rule1=re.compile(r'^([a-zA-Z0-9][a-zA-Z0-9\-\_\.]*?[a-zA-Z0-9])\@([a-zA-Z0-9][a-zA-Z0-9\-\_\.]*?[a-zA-Z0-9])\.(com|cn|org|net|us)$')
+        self.rule2=re.compile(r'^<([a-zA-Z]+\s?[a-zA-Z]+)>\s*([a-zA-Z0-9][a-zA-Z0-9\-\_\.]*?[a-zA-Z0-9])\@([a-zA-Z0-9][a-zA-Z0-9\-\_\.]*?[a-zA-Z0-9])\.(com|cn|org|net|us)$')
+    def check(self,address):
+        if self.match(self.rule1,address):
+            print '%s is an e-mail address.' % address
+        elif self.match(self.rule2,address):
+            print "%s is an e-mail address and name is %s" % (address,self.rule2.match(address).group(1))
         else:
-            return "Usage %s <version>(1|2) <e-mail address>" % sys.argv[0]
+            print "%s is not an e-mail address" % address
 
     def match(self,rule,address):
         if rule.match(address):
-            return "is"
+            return True
         else:
-            return "is not"
-
+            return False
 r=e_mail()
-r.check(sys.argv[1],sys.argv[2])
+#print r.match(r.rule1,sys.argv[1])
+#print r.match(r.rule2,sys.argv[1])
+#print r.rule2.match(sys.argv[1]).group(1)
+r.check(sys.argv[1])
 
