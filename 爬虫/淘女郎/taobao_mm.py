f682a6c7d2dfe031b3b71370cc25fa0542b7e67f
clessli <316600949@qq.com>
2018-04-15 17:50:13 +0800

    ready for save into database

--- 爬虫/淘女郎/taobao_mm.py
+++ 爬虫/淘女郎/taobao_mm.py
@@ -4,47 +4,54 @@ import json
 import urllib2
 #doc = pq(url='https://mm.taobao.com/search_tstar_model.htm?spm=719.6642093.1998089564.7.WIj7L7')
 #print doc('div.lady-girls-wrap.tb_mm_main ul.girls-list')
-def save_Img(img_url,file_name):
-    if 'jpeg' in img_url:
-        file_suffix = '.jpeg'
-    elif 'jpg' in img_url:
-        file_suffix = '.jpg'
-    elif 'png' in img_url:
-        file_suffix = '.png'
-    else:
-        file_suffix = '.bmp'
-    cat_img = urllib2.urlopen(img_url).read()
-    with open(file_name + file_suffix,'wb') as img:
-        img.write(cat_img)
 
-def parse_data(url):
-    doc = pq(url=url)
-    data = json.loads(doc('p').text())
-    page_status = data['status']
-    totalPage = data['data']['totalPage']
-    page = data['data']['currentPage']
-    while page_status == 1 and page <= totalPage:
-            img_list = data['data']['searchDOList']
-            page += 1
-            for item in img_list:
-                name = item['realName']
-                city = item['city']
-                height = item['height']
-                weight = item['weight']
-                img_url = 'https:'+item['avatarUrl']
-                file_name = '%s_%s_%skg_%scm' % (name,city,weight,height)
-                print file_name,img_url
-                save_Img(img_url,file_name)
-            print page_status,page
-            url_new = url + '&currentPage=' + str(page)
-            print url_new
-            doc = pq(url=url_new)
-            data = json.loads(doc('p').text())
-            page_status = data['status']
+class TaoGirl():
+    def __init__(self,url):
+        self.index_url = url
+
+    def save_Img(self,img_url,file_name):
+        if 'jpeg' in img_url:
+            file_suffix = '.jpeg'
+        elif 'jpg' in img_url:
+            file_suffix = '.jpg'
+        elif 'png' in img_url:
+            file_suffix = '.png'
+        else:
+            file_suffix = '.bmp'
+        self.cat_img = urllib2.urlopen(img_url).read()
+        with open(file_name + file_suffix,'wb') as self.img:
+            self.img.write(self.cat_img)
+
+    def parse_data(self):
+        self.doc = pq(url=self.index_url)
+        self.data = json.loads(self.doc('p').text())
+        self.page_status = self.data['status']
+        self.totalPage = self.data['data']['totalPage']
+        self.page = self.data['data']['currentPage']
+        #while self.page_status == 1 and self.page <= self.totalPage:
+        while self.page_status == 1 and self.page <= 1:
+                self.img_list = self.data['data']['searchDOList']
+                self.page += 1
+                for item in self.img_list:
+                    self.name = item['realName']
+                    self.city = item['city']
+                    self.height = item['height']
+                    self.weight = item['weight']
+                    self.img_url = 'https:'+item['avatarUrl']
+                    self.file_name = '%s_%s_%skg_%scm' % (self.name,self.city,self.weight,self.height)
+                    print 'Downloading file:%s, url:%s' % (self.file_name,self.img_url)
+                    #self.save_Img(self.img_url,self.file_name)
+                self.url_new = self.index_url + '&currentPage=' + str(self.page)
+                print 'The url of the next page is %s(page number:%s, page status:%s)' % (self.url_new,self.page,self.page_status)
+                self.doc = pq(url=self.url_new)
+                self.data = json.loads(self.doc('p').text())
+                self.page_status = self.data['status']
 
 if __name__ == '__main__':
     tao_girl_url = 'https://mm.taobao.com/tstar/search/tstar_model.do?_input_charset=utf-8'
-    parse_data(tao_girl_url)
+    taogirl_data=TaoGirl(tao_girl_url)
+    print taogirl_data.index_url
+    taogirl_data.parse_data()
 #totalPage = tg_dict_data['totalPage']
 #    city = data['city']
 #data = tg_dict_data['searchDOList'][0]
