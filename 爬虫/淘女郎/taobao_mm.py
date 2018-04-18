ea9ce3d1e2f6b124be620e88b5f44636c7b02aea
clessli <316600949@qq.com>
2018-04-18 14:23:27 +0800

    利用生成器减少内存开销

--- 爬虫/淘女郎/taobao_mm.py
+++ 爬虫/淘女郎/taobao_mm.py
@@ -3,8 +3,6 @@
 from pyquery import PyQuery as pq
 from pymongo import MongoClient
 import json , urllib2 , time
-#doc = pq(url='https://mm.taobao.com/search_tstar_model.htm?spm=719.6642093.1998089564.7.WIj7L7')
-#print doc('div.lady-girls-wrap.tb_mm_main ul.girls-list')
 
 class TaoGirl():
     def __init__(self,url):
@@ -15,9 +13,8 @@ class TaoGirl():
         self.db_user = 'worker'
         self.db_user_pwd = 'more_work'
         self.mongodb_conn(self.db_host,self.db_name,self.col_name,self.db_user,self.db_user_pwd)
-        self.doc = pq(url=self.index_url)
-        self.data = json.loads(self.doc('p').text())
-        self.page_status = self.data['status']
+        self.c_page = 1
+        self.row = 0
 
     def mongodb_conn(self,db_host,db_name,col_name,db_user,db_user_pwd):
         self.MONGO_CONN = MongoClient(db_host)
@@ -33,65 +30,63 @@ class TaoGirl():
                 upsert = True
             )
 
-    def save_Img(self,img_url,file_name):
-        if 'jpeg' in img_url:
-            file_suffix = '.jpeg'
-        elif 'jpg' in img_url:
-            file_suffix = '.jpg'
-        elif 'png' in img_url:
-            file_suffix = '.png'
-        else:
-            file_suffix = '.bmp'
-        self.cat_img = urllib2.urlopen(img_url).read()
-        self.file_name = file_name + file_suffix
-        self.db_document['filename'] = self.file_name
-        with open(self.file_name,'wb') as self.img:
-            self.img.write(self.cat_img)
-            self.mongodb_update(self.col_name,self.db_document)
+    def save_Img(self,save_data,col_name):
+        img_url = save_data.get('db_document').get('img_url')
+        file_name = save_data.get('file_name')
+        db_document = save_data.get('db_document')
+        cat_img = urllib2.urlopen(img_url).read()
+        db_document['filename'] = file_name
+        with open(file_name,'wb') as img:
+            img.write(cat_img)
+            self.mongodb_update(col_name,db_document)
 
-    def parse_data(self):
-        self.totalPage = self.data['data']['totalPage']
-        self.page = self.data['data']['currentPage']
-        #while self.page_status == 1 and self.page <= self.totalPage:
-        while self.page_status == 1 and self.page <= 1:
-                self.img_list = self.data['data']['searchDOList']
-                self.page += 1
-                for item in self.img_list:
-                    self.userid = item['userId']
-                    self.name = item['realName']
-                    self.city = item['city']
-                    self.height = item['height']
-                    self.weight = item['weight']
-                    self.img_url = 'https:'+item['avatarUrl']
-                    self.file_name = '%s_%s_%skg_%scm' % (self.name,self.city,self.weight,self.height)
-                    self.db_document = {'userid':self.userid,
-                                        'name':self.name,
-                                        'city':self.city,
-                                        'height':self.height,
-                                        'weight':self.weight,
-                                        'img_url':self.img_url}
-                    print 'Downloading file:%s, url:%s' % (self.file_name,self.img_url)
-                    self.save_Img(self.img_url,self.file_name)
-                self.url_new = self.index_url + '&currentPage=' + str(self.page)
-                #time.sleep(1)
-                print 'The url of the next page is %s(page number:%s, page status:%s)' % (self.url_new,self.page,self.page_status)
-                try:
-                    self.doc = pq(url=self.url_new)
-                except:
-                    return False
-                self.data = json.loads(self.doc('p').text())
-                self.page_status = self.data['status']
+    def parse_data(self,url,page_max):
+        page_status = 1
+        while page_status == 1 and self.c_page <= page_max:
+            doc = pq(url=url)
+            data = json.loads(doc('p').text())
+            page_status = data['status']
+            total_page = data['data']['totalPage']
+            page_max = page_max if page_max<total_page else total_page
+            for item in data['data']['searchDOList']:
+                userid = item['userId']
+                name = item['realName']
+                city = item['city']
+                height = item['height']
+                weight = item['weight']
+                img_url = 'https:' + item['avatarUrl']
+                file_title = '%s_%s_%skg_%scm' % (name, city, weight, height)
+                file_name = file_title + '.' + img_url.split('.')[-1]
+                yield {
+                    'file_name':file_name,
+                    'db_document':{
+                       'userid':userid,
+                        'name':name,
+                        'city':city,
+                        'height':height,
+                        'weight':weight,
+                        'img_url':img_url
+                    }
+                }
+                self.c_page = data['data']['currentPage'] + 1
+                url = self.index_url + '&currentPage=' + str(self.c_page)
+
+    def save_data(self,page_max = 99999999,row_max = 999999999999):
+        gen_data = self.parse_data(self.index_url,page_max)
+        if gen_data:
+            for save_data in gen_data:
+                print 'Downloading file:%s, url:%s' % (save_data.get('file_name'),save_data.get('db_document').get('img_url'))
+                self.save_Img(save_data,self.col_name)
+                print 'Download successful.[%s]' % save_data.get('db_document').get('userid')
+                self.row += 1
+                if self.row >= row_max:
+                    break
+
 
 if __name__ == '__main__':
     tao_girl_url = 'https://mm.taobao.com/tstar/search/tstar_model.do?_input_charset=utf-8'
     taogirl_data=TaoGirl(tao_girl_url)
     print taogirl_data.index_url
-    taogirl_data.parse_data()
-#totalPage = tg_dict_data['totalPage']
-#    city = data['city']
-#data = tg_dict_data['searchDOList'][0]
-#print data
-#print img_url
-#print file_name
-#print type(img_url)
-#print "%s %s" % (Img_url,city)
+    page_max = 1
+    row_max = 10
+    taogirl_data.save_data(page_max,row_max)
