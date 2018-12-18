3a04e47ecb5b7cd44826fcae48ee5ed8a8291df3
clessli <316600949@qq.com>
2018-12-18 17:29:22 +0800

    update:cpu_load

--- Tools/over_watch/bin/base_info.py
+++ Tools/over_watch/bin/base_info.py
@@ -1,73 +1,77 @@
-# -*- coding : utf-8 -*-
+# encoding:utf-8
 
 
-from collections import OrderedDict
+#from collections import OrderedDict
 
-import aiofiles
+#import aiofiles
 import asyncio
 import asyncssh
+#import sys
 
-async def ssh_by_keys(hostname, username, keys_filename, cmd):
-    async with asyncssh.auth_keys.read_authorized_keys(keys_filename) as auth_keys:
-        async with asyncssh.connect(hostname, username=username, client_keys=[auth_keys]) as conn:
-            result = await conn.run(cmd, check=True)
-            return result.stdout
+
+async def ssh_by_keys(hostname, username, cmd):
+    async with asyncssh.connect(hostname, username=username) as conn:
+        result = await conn.run(cmd, check=True)
+        #print(result.stdout, end='')
+        return result.stdout
+
 
 class CPUinfo():
-    def __init__(self, hostname, username, keys_filename, cmd='cat /proc/CPUinfo'):
-        self.CPUinfo = OrderedDict()
-        self.procinfo = OrderedDict()
+    def __init__(self, hostname, username, cmd='sar 5 6'):
         self.hostname = hostname
         self.username = username
-        self.keys_filename = keys_filename
-        self.cmd = cmd
-        self.nprocs = 0
+        self._cmd = cmd
+        self.cpu_iowait = None
+        self.cpu_idle = None
 
     async def get_info(self):
-        result = await ssh_by_keys(self.hostname,self.username,self.keys_filename,self.cmd)
-        for line in result:
-            if not line.strip():
-                # end of one processor
-                self.CPUinfo['proc%s' % self.nprocs] = self.procinfo
-                self.nprocs = self.nprocs + 1
-                # Reset
-                self.procinfo = OrderedDict()
-            else:
-                if len(line.split(':')) == 2:
-                    self.procinfo[line.split(':')[0].strip()] = line.split(':')[1].strip()
-                else:
-                    self.procinfo[line.split(':')[0].strip()] = ''
+        result = await ssh_by_keys(self.hostname, self.username, self._cmd)
+        cpuinfo_list = result.strip().split('\n')
+        if cpuinfo_list[-1].strip():
+            self.cpu_iowait = cpuinfo_list[-1].split()[-3]
+            self.cpu_idle = cpuinfo_list[-1].split()[-1]
 
 
 class OVER_WATCH():
-    def __init__(self, filename, ssh_filename):
-        self.filename = filename
-        self.ssh_filename = ssh_filename
+    def __init__(self, filename, username='root'):
+        self._tasks = []
+        self._host_cpuinfo = {}
+        self.username = username
+        self._hostlist = self._read_filelist(filename)
 
-    async def read_hostlist(self, filename):
+    def _read_filelist(self, filename):
         try:
-            async with aiofiles.open(filename) as hostlist:
-                return await hostlist
+            with open(filename, 'r') as fp:
+                return fp.readlines()
         except FileNotFoundError as err:
             print(err)
             exit()
 
-    async def get_cpuinfo_tasks(self, filename, ssh_filename, cmd='cat /proc/CPUinfo'):
-        hostlist = await self.read_hostlist(filename)
-        tasks = [hostlist]
-        for host in hostlist:
-            host_cpuinfo = CPUinfo(host, 'root', ssh_filename, cmd)
-            task = await host_cpuinfo.get_info()
-            tasks.append(task)
-        return tasks
+    def _add_corountines_tasks(self, c_obj):
+        self._tasks.append(c_obj)
 
-    def main(self):
-        cpuinfo_tasks = self.get_cpuinfo_tasks(self.filename, self.ssh_filename)
+    def _get_cpuinfo_tasks(self, cmd='sar 5 6'):
+        for host in self._hostlist:
+            self._host_cpuinfo[host] = CPUinfo(host, self.username, cmd)
+            self._add_corountines_tasks(self._host_cpuinfo[host].get_info())
+
+    def main(self, cmd):
+        self._get_cpuinfo_tasks(cmd)
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.wait(cpuinfo_tasks))
+        loop.run_until_complete(asyncio.wait(self._tasks))
         loop.close()
+        cpuinfo_dict = {}
+        for host in self._hostlist:
+            if self._host_cpuinfo[host].cpu_iowait or self._host_cpuinfo[host].cpu_idle:
+                print('host:%s iowait:%s idle:%s' % (host,
+                                                     self._host_cpuinfo[host].cpu_iowait,
+                                                     self._host_cpuinfo[host].cpu_idle))
+                cpuinfo_dict[host.strip('\n')] = dict(iowait=self._host_cpuinfo[host].cpu_iowait,
+                                                      idle=self._host_cpuinfo[host].cpu_idle)
+        return cpuinfo_dict
 
 
 if __name__ == '__main__':
-
-
+    hostlist_path = '/home/cless/hosts.list'
+    overwatch = OVER_WATCH(hostlist_path)
+    print(overwatch.main('sar 1 1'))
